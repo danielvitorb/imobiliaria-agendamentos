22d6b7341d616f8b4660ecd01abcbf5d59a3d5c8
Daniel Vítor <danielvitoroliveirab@gmail.com>
2025-07-03 14:10:05 -0300

    imovel

--- imovel.h
+++ imovel.h
@@ -1,7 +1,29 @@
-class Imovel {
-    private:
-        double preco;
-        
-    public:
+#include <string>
+using namespace std;
 
+class Imovel {
+    private: // Atributos private
+        static int nextId;
+        int id;
+        int proprietarioId;
+        double preco;
+        double lat;
+        double lng;
+        string endereco;
+
+    public: // Construtor e métodos public
+        Imovel();
+
+        void setPropietarioId(int id);
+        void setPreco(double preco);
+        void setLatitude(double latitude);
+        void setLongitude(double longitude);
+        void setEndereco(string endereco);
+
+        int getId();
+        int getProprietarioId();
+        double getPreco();
+        double getLatitude();
+        double getLongitude();
+        string getEndereco();
 };
