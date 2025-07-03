da447fbd47def66392c51b2a7ab89baaa66c9f21
Daniel Vítor <danielvitoroliveirab@gmail.com>
2025-07-03 18:09:36 -0300

    main

--- main.cpp
+++ main.cpp
@@ -5,6 +5,30 @@
 #include "imovel.h"
 
 int main(){
+    int numCorretores, avaliador, numClientes;
+    double lat, lon;
+    string telefone, nome;
+    vector<Corretor> corretores; 
+    vector<Corretor> avaliadores;
 
+    cin >> numCorretores;
+    for(int i = 0; i < numCorretores; i++){
+        cin >> telefone >> avaliador >> lat >> lon;
+        getline(cin >> std::ws, nome);
+
+        Corretor corretor(telefone, avaliador, lat, lon, nome);
+        corretores.push_back(corretor);
+
+        if(corretores[i].getAvaliador()){
+            avaliadores.push_back(corretores[i]);
+        }
+    }
+
+    cin >> numClientes;
+    for(int j = 0; j < numClientes; j++){
+        cin >> telefone;
+        getline(cin >> std::ws, nome);
+        Cliente cliente(telefone, nome);
+    }
     return 0;
 }
