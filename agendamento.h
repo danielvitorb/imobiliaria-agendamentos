cdf7ec92e018d5eab763e6faf1e9df66de80e6fa
Daniel Vítor <danielvitoroliveirab@gmail.com>
2025-07-14 09:04:28 -0300

    Update agendamento.h

--- agendamento.h
+++ agendamento.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include "corretor.h"  
 
+// Declaração dos métodos de agendamento
 double haversine(double lat1, double lon1, double lat2, double lon2);
 void gerarAgendamentos(vector<Corretor>& avaliadores, vector<Imovel>& imoveis);
 int maisPerto(Corretor &avaliador, double lat1, double long1);
